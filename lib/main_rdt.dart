ce0aac4c29dfe68beb4e083fa896cb79b5193f1d
Nick Meinhold <nick.meinhold@gmail.com>
2020-01-05 20:17:32 +1100

    Add entrypoint for remotedevtools
    - added main_rdt.dart and new launch_config
    
    fixes #17

--- lib/main_rdt.dart
+++ lib/main_rdt.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart' hide Action;
 import 'package:google_sign_in/google_sign_in.dart';
@@ -7,14 +9,19 @@ import 'package:podcustard/redux/app_reducer.dart';
 import 'package:podcustard/redux/middleware.dart';
 import 'package:podcustard/services/auth_service.dart';
 import 'package:podcustard/services/itunes_service.dart';
+import 'package:podcustard/utilities/mocks.dart';
 import 'package:podcustard/widgets/app.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_remote_devtools/redux_remote_devtools.dart';
 
 void main() async {
+  final remoteDevtools = RemoteDevToolsMiddleware(imac_ip22);
+
   final store = Store<AppState>(
     appReducer,
     initialState: AppState.init(),
     middleware: [
+      remoteDevtools,
       ...createMiddleware(
         AuthService(
             FirebaseAuth.instance, GoogleSignIn(scopes: <String>['email'])),
@@ -22,6 +29,14 @@ void main() async {
       ),
     ],
   );
+
+  remoteDevtools.store = store;
+
+  try {
+    await remoteDevtools.connect();
+  } on SocketException catch (e) {
+    print(e);
+  }
 
   runApp(PodcustardApp(store));
 }
